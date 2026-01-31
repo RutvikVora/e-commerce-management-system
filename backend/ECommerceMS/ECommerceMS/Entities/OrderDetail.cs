95c681e9f1594f20961d36990cfbcb278c9e9dcf
RutvikVora <rutvikvora29@gmail.com>
2026-01-31 14:16:35 +0530

    Initial Setup with scaffolding.

---
+++ backend/ECommerceMS/ECommerceMS/Entities/OrderDetail.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ECommerceMS.Entities;
+
+public partial class OrderDetail
+{
+    public int OrderId { get; set; }
+
+    public int? ProductId { get; set; }
+
+    public int Quantity { get; set; }
+
+    public decimal? TotalPrice { get; set; }
+
+    public DateTime OrderDate { get; set; }
+
+    public DateTime CreatedDate { get; set; }
+
+    public DateTime UpdatedDate { get; set; }
+
+    public virtual ProductDetail? Product { get; set; }
+}
---
+++ backend/ECommerceMS/ECommerceMS/Entities/ProductDetail.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ECommerceMS.Entities;
+
+public partial class ProductDetail
+{
+    public int ProductId { get; set; }
+
+    public string Name { get; set; } = null!;
+
+    public decimal Price { get; set; }
+
+    public int Stock { get; set; }
+
+    public DateTime CreatedDate { get; set; }
+
+    public DateTime UpdatedDate { get; set; }
+
+    public virtual ICollection<OrderDetail> OrderDetails { get; set; } = new List<OrderDetail>();
+}
